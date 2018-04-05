7ac16105dfad00f18395b1a1c0454923466cb21d
Arthur C Edwards <Arthur.Edwards321@gmail.com>
2018-04-05 15:30:04 -0400

    nav installed . Still need to style the hamburger icon in the top right

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+// For Sticky Nav
+
+$('.title-bar').on('sticky.zf.stuckto:top', function(){
+  $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function(){
+  $(this).removeClass('shrink');
+})
+
+// end of the sticky Js component
